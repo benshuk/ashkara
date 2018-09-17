16cda07881960b3c1efde05bb7874ba876cbe32d
emmanuel-ferdman <emmanuel-ferdman@users.noreply.github.com>
2018-09-18 01:01:16 +0300

    Ashkara: Creating consts file and updating the title/background selector algo

---
+++ ashkaraConsts.js
@@ -0,0 +1,9 @@
+// Background and titles
+const ASHKARA_BACKGROUNDS = ["#FF5A5A", "#ffb25a", "#19c78d","#ef476f","#ffd166","#06d6a0","#118ab2","#073b4c", "#114b5f", "#028090", "#456990", "#f45b69"];
+const ASHKARA_TITLES = ["בדוק", "קללל", "תכל'ס", "חד משמעית", "ברור", "וואלה", "וואי וואי", "מפחיד", "פחות"];
+
+// Speed and timeout
+const DEFAULT_SPEED = 125;
+const QUICKEST_SPEED = 0;
+const ERASE_TIMEOUT = 2000;
+const TYPE_TIMEOUT = 125;
--- index.js
+++ index.js
@@ -1,60 +1,58 @@
 let index = 0;
-let speed = 125;
-let eraseTimeout = 2000;
-let typeTimeout = 125;
 let choose = arr => arr[Math.floor(Math.random() * arr.length)];
-let titles = ["בדוק", "קללל", "תכל'ס", "חד משמעית", "ברור", "וואלה", "וואי וואי"];
-let backgrounds = ["#FF5A5A", "#ffb25a", "#19c78d","#ef476f","#ffd166","#06d6a0","#118ab2","#073b4c", "#114b5f", "#028090", "#456990", "#f45b69"];
+let subTitles = ASHKARA_TITLES.slice();
+let subBg = ASHKARA_BACKGROUNDS.slice();
 let ariaHiddenElement = '<span aria-hidden="true"></span>';
 
 // Init background
-let bg = choose(backgrounds);
+let bg = choose(subBg);
 document.body.style.backgroundColor = bg;
 
 // Init title
-let title = choose(titles);
-setTimeout(type,speed);
+let title = choose(subTitles);
+setTimeout(type,DEFAULT_SPEED);
 
 // Type the title
 function type() {
     if(index < title.length) {
+        var currentChar = title.charAt(index + 1);
+        var currentSpeed = (currentChar === " ") ? QUICKEST_SPEED : DEFAULT_SPEED;
         document.getElementById("title").innerHTML = title.substring(0, index + 1) + ariaHiddenElement;
         index++;
-        setTimeout(type, speed);
+        setTimeout(type, currentSpeed);
     } else {
-        setTimeout(erase, eraseTimeout);
+        setTimeout(erase, ERASE_TIMEOUT);
     }
 }
 
 // Erase the title
 function erase() {
     if(index >= 0) {
-        var temp = title.substring(0,index);
+        var currentChar = title.charAt(index);
+        var currentSpeed = (currentChar === " ") ? QUICKEST_SPEED : DEFAULT_SPEED;
         document.getElementById("title").innerHTML = title.substring(0, index) + ariaHiddenElement;
         index--;
-        setTimeout(erase, speed);
+        setTimeout(erase, currentSpeed);
     } else {
         updatePage();
-        setTimeout(type, typeTimeout);
+        setTimeout(type, TYPE_TIMEOUT);
     }
 }
 
 // Update the title and the background of the page, without repeating
 function updatePage() {
-    // Set new title from the list of titles
-    var index = titles.indexOf(title);
-    if (index !== -1) {
-        var subTitles = titles.slice();
-        subTitles.splice(index, 1);
-        title = choose(subTitles);
+    // Set new title from the sublist of titles
+    if(subTitles.length == 1) {
+        subTitles = ASHKARA_TITLES.slice();
     }
+    subTitles.splice(subTitles.indexOf(title), 1);
+    title = choose(subTitles);
 
-    // Set new background from the list of backgrounds
-    var index = backgrounds.indexOf(bg);
-    if (index !== -1) {
-        var subBg = backgrounds.slice();
-        subBg.splice(index, 1);
-        bg = choose(subBg);
-        document.body.style.backgroundColor = bg;
+    // Set new background from the sublist of backgrounds
+    if(subBg.length == 1) {
+        subBg = ASHKARA_BACKGROUNDS.slice();
     }
+    subBg.splice(subBg.indexOf(bg), 1);
+    bg = choose(subBg);
+    document.body.style.backgroundColor = bg;
 }
