a407bd41206452801995e74e49a2d64008abe08c
emmanuel-ferdman <emmanuel-ferdman@users.noreply.github.com>
2018-09-18 23:50:52 +0300

    Ashkara: Fixing cookie bug

--- index.js
+++ index.js
@@ -180,16 +180,17 @@ function baduckAchiveCheck() {
 
 /** ---------- Coockies Function ---------- **/
 
-function setCookie(cname, cvalue, exdays) {
+function setCookie(cname,cvalue,exdays) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
     var name = cname + "=";
-    var ca = document.cookie.split(';');
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
     for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
@@ -204,7 +205,7 @@ function getCookie(cname) {
 
 function updateCookie(cname,cvalue) {
     var value = getCookie(cname);
-    if (cvalue > value || cvalue == "") {
+    if (value == "" || cvalue > Number(value)) {
         setCookie(cname, cvalue, 365);
     }
 }
